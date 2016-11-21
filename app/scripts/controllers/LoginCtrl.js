f1081213c385226a9ade742406f17e3920330038
Viorica Gosa <vivianagosa@gmail.com>
2016-11-21 12:36:34 +0100

    Connect only non- empty usernames

--- app/scripts/controllers/LoginCtrl.js
+++ app/scripts/controllers/LoginCtrl.js
@@ -8,22 +8,31 @@
  * Controller of the wordPuzzleGameApp
  */
  angular.module('wordPuzzleGameApp')
- .controller('LoginCtrl', ["$scope", "$location","DynamicDataService", function ($scope, $location, DynamicDataService) {
+ .controller('LoginCtrl', ["$rootScope", "$location","DynamicDataService", function ($rootScope, $location, DynamicDataService) {
     DynamicDataService.connectToDatabase();
 
+    this.invalidUsername = false;
     this.connectUser = function(username) {
-        //connect existing user or create new user
-        DynamicDataService.getUser(username).then(
-            function (res) {
-                //after connect, redirect page to the main page,
-                //to play the game
-                $location.path("wordpuzzle");
-            },
-            function (err) {
-                console.log(err);
-            }
-            );
-    };
+        //make sure not to submit an empty username
+        if(username) {
+            //make the username visible on the rootScope
+            $rootScope.username = username;
+            //connect existing user or create new user
+            DynamicDataService.getUser(username).then(
+                function (res) {
+                    //after connect, redirect page to the main page,
+                    //to play the game
+                    $location.path("wordpuzzle");
+                },
+                function (err) {
+                    console.log(err);
+                }
+                );
+        } else {
+            this.invalidUsername = true;
+        }
+};
 
 
-  }]);
+
+}]);
