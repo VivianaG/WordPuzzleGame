f994eeabbe72e455c2b69e61a3ec488e149d070b
Viorica Gosa <vivianagosa@gmail.com>
2016-11-22 00:07:04 +0100

    Added functionalities for playing a game and update dynamically the score for an user

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,24 +1,28 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name wordPuzzleGameApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the wordPuzzleGameApp
- */
- angular.module('wordPuzzleGameApp')
- .controller('MainCtrl', ["$rootScope", "$scope", "$location", "$interval", "$timeout", "DynamicDataService",
- 	function ($rootScope, $scope, $location, $interval, $timeout, DynamicDataService) {
+* @ngdoc function
+* @name wordPuzzleGameApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the wordPuzzleGameApp
+*/
+angular.module('wordPuzzleGameApp')
+.controller('MainCtrl', ["$rootScope", "$scope", "$location", "$interval", "$timeout", "DynamicDataService",
+	function ($rootScope, $scope, $location, $interval, $timeout, DynamicDataService) {
 
 	var words = [];
 	var remainedWords = [];
 	var self = this;
+	var wordEvaluation = "";
+	var wordScore = 0;
 
 	$scope.guessedWord = "";
+	this.roundScore = 0;
 	this.currentWord = {};
 	this.user = $rootScope.user;
 	this.gameStarted = false;
+	this.gameEnded = false;
 	this.score = 0;
 	this.leftTime = 40;
 	this.stopTimer = null;
@@ -35,18 +39,27 @@
 
 	this.startGame = function() {
 		this.gameStarted = true;
+		this.gameEnded = false;
 		this.shouldCheckWord = false;
+		this.roundScore = 0;
 		getNextWord();
 		this.stopTimer = startCountdown();
 	};
 
 	this.endGame = function() {
 		this.gameStarted = false;
+		this.gameEnded = true;
 		this.shouldCheckWord = false;
 		if(this.stopTimer) {
 			stopCountdown();
 		}
 		resetCountdown();
+
+	//make sure to update the server score for the user
+	//if the round score is greater than the server side score for the user
+	if(this.roundScore > $rootScope.user.score) {
+		updateUserScore();
+	}
 	}
 
 	this.onGetNextWord = function() {
@@ -55,6 +68,36 @@
 
 	this.onWordSubmit = function() {
 		evaluateWord();
+	}
+
+	this.onWordKeyDown = function(event) {
+	//save the word before possible delete
+	wordEvaluation = self.guessedWord;
+	}
+
+	this.onWordKeyUp = function(event) {
+		let key = event.keyCode || event.charCode;
+		let noOfDelLetters = wordEvaluation.length - self.guessedWord.length;
+
+		/*if the user deletes one ore more characters, then update the score for a word*/
+		if(noOfDelLetters > 0 && (key === 8 || key === 46)) {
+			//update the score if deleting
+			if(wordScore > 0)
+				wordScore -= noOfDelLetters;
+		}
+		//check for 'enter' key
+		if(key === 13) {
+			//check if it is the correct word
+			evaluateWord();
+		}
+		else if(key === 39) {
+			this.onGetNextWord();
+		}
+	}
+
+	function updateUserScore() {
+		DynamicDataService.updateUserScore($rootScope.user.username, self.roundScore);
+		$rootScope.user.score = self.roundScore;
 	}
 
 	function getHighScores() {
@@ -86,14 +129,19 @@
 
 	function getNextWord() {
 		self.shouldCheckWord = false;
- 		self.isWordCorrect = false;
+		self.isWordCorrect = false;
 		self.guessedWord = "";
 
 		//make sure we have another word in queue
 		if(remainedWords  && remainedWords.length > 0) {
 			self.currentWord = remainedWords[0];
+
+			let currentWordLength = self.currentWord.correctWord.length;
+			wordScore = Math.floor(Math.pow(1.95, currentWordLength / 3));
+
 			remainedWords.shift();
-		} else {
+		}
+		else {
 			//reset the list of words
 			refreshWordList();
 		}
@@ -102,14 +150,19 @@
 	function evaluateWord() {
 		self.shouldCheckWord = true;
 
+		//always make sure that the score has positive value
+		if(wordScore < 0)
+			wordScore = 0;
 		if(self.guessedWord === self.currentWord.correctWord) {
 			self.isWordCorrect = true;
+			self.roundScore += wordScore;
 
 			//keep styling for success active some time
 			$timeout(function() {
 				getNextWord();
 			}, 300);
-		} else {
+		}
+		else {
 			self.isWordCorrect = false;
 		}
 	}
@@ -126,10 +179,12 @@
 		var interval = $interval(function() {
 			if(self.leftTime > 0) {
 				updateCountdown();
-			} else {
-				stopCountdown();
 			}
-	}, 1000);
+			else {
+				stopCountdown();
+				self.endGame();
+			}
+		}, 1000);
 		return interval;
 	}
 
@@ -144,4 +199,4 @@
 	function updateCountdown() {
 		self.leftTime -= 1;
 	}
- }]);
+}]);
