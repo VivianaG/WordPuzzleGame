a031797580ff2d60367704624a7fd549c44a7c99
Viorica Gosa <vivianagosa@gmail.com>
2016-11-21 20:27:28 +0100

    Add functionality to countdown timer

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -10,6 +10,7 @@
  angular.module('wordPuzzleGameApp')
  .controller('MainCtrl', ["$rootScope", "$scope", "$location", "$interval", "DynamicDataService",
  	function ($rootScope, $scope, $location, $interval, DynamicDataService) {
+
 	var words = [];
 	var remainedWords = [];
 	var self = this;
@@ -19,6 +20,7 @@
 	this.gameStarted = false;
 	this.score = 0;
 	this.leftTime = 40;
+	this.stopTimer = null;
 
 	if(!$rootScope.user) {
 		//go to login page
@@ -29,13 +31,19 @@
 	getWords();
 
 	this.startGame = function() {
-		getWords();
 		this.gameStarted = true;
 
+		getNextWord();
+		this.stopTimer = startCountdown();
 	};
 
 	this.endGame = function() {
 		this.gameStarted = false;
+
+		if(this.stopTimer) {
+			stopCountdown();
+		}
+		resetCountdown();
 	}
 
 	this.onGetNextWord = function() {
@@ -76,11 +84,38 @@
 			remainedWords.shift();
 		} else {
 			//reset the list of words
-			remainedWords = makeArrayCopy(words);
+			refreshWordList();
 		}
+	}
+
+	function refreshWordList() {
+		remainedWords = makeArrayCopy(words);
 	}
 
 	function makeArrayCopy(arrayToCopy) {
 		return arrayToCopy.slice(0);
+	}
+
+	function startCountdown() {
+		var interval = $interval(function() {
+			if(self.leftTime > 0) {
+				updateCountdown();
+			} else {
+				stopCountdown();
+			}
+	}, 1000);
+		return interval;
+	}
+
+	function stopCountdown() {
+		$interval.cancel(self.stopTimer);
+	}
+
+	function resetCountdown() {
+		self.leftTime = 40;
+	}
+
+	function updateCountdown() {
+		self.leftTime -= 1;
 	}
  }]);
